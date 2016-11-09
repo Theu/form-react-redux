0c693221e6518bed8fe09b31788794788e659002
Matteo Contini <matteo.contini@zalando-lounge.de>
2016-11-09 17:52:12 +0100

    created the first react component with ProTypes list

---
+++ src/Components/Form.js
@@ -0,0 +1,21 @@
+import React, {PropTypes} from 'react';
+
+class Form extends React.Component {
+  render() {
+    return(
+      <form>
+        {this.props.children}
+      </form>
+    );
+  }
+}
+
+PropTypes: {
+  children: PropTypes.node,
+  values: PropTypes.object,
+  update: PropTypes.func,
+  reset: PropTypes.func,
+  onSubmit: PropTypes.func,
+}
+
+export default Form
